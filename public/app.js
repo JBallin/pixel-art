e190542509885a82abe4cc026617cbe85f0d1b2c
JBallin <JBallin@users.noreply.github.com>
2018-12-11 22:35:41 -0800

    Refactor and add visual of current square

--- public/app.js
+++ public/app.js
@@ -1,14 +1,16 @@
-const numRows = 10;
-const color = "red";
+const size = 10;
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+let color = colors[0];
 let isDrawing = false;
+let prevColor = 'white';
 
 window.onload = () => {
   const canvas = document.querySelector('.canvas');
   const palette = document.querySelector('.palette');
-  buildCanvas(canvas);
+  buildCanvas(canvas, size);
 }
 
-const buildCanvas = (div) => {
+const buildCanvas = (div, numRows) => {
   for (let row = 0; row < numRows; row++) {
     const row = document.createElement('tr');
     for (let column = 0; column < numRows; column++) {
@@ -24,11 +26,21 @@ const addMouseEvents = (el) => {
   el.onmousedown = () => {
     isDrawing = true;
     el.style.backgroundColor = color;
+    prevColor = color;
   }
   el.onmouseup = () => {
     isDrawing = false;
   }
   el.onmouseenter = () => {
-    if (isDrawing) el.style.backgroundColor = color;
+    if (isDrawing) {
+      el.style.backgroundColor = color;
+      prevColor = color;
+    } else {
+      prevColor = el.style.backgroundColor;
+      el.style.backgroundColor = 'lightgray';
+    }
+  }
+  el.onmouseleave = () => {
+    if (!isDrawing) el.style.backgroundColor = prevColor;
   }
 }
