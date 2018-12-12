1629d5ddfb08419155db9979b6fb10457ecd9208
JBallin <JBallin@users.noreply.github.com>
2018-12-11 22:48:55 -0800

    Add palette/refactor

--- public/app.js
+++ public/app.js
@@ -1,46 +1,60 @@
 const size = 10;
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-let color = colors[0];
+const brushColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white'];
+
+let brushColor = brushColors[0];
 let isDrawing = false;
-let prevColor = 'white';
 
 window.onload = () => {
   const canvas = document.querySelector('.canvas');
   const palette = document.querySelector('.palette');
   buildCanvas(canvas, size);
+  buildPalette(palette, brushColors);
 }
 
-const buildCanvas = (div, numRows) => {
+const buildCanvas = (table, numRows) => {
   for (let row = 0; row < numRows; row++) {
     const row = document.createElement('tr');
     for (let column = 0; column < numRows; column++) {
       const pixel = document.createElement('td');
-      addMouseEvents(pixel);
+      addPixelMouseEvents(pixel);
       row.appendChild(pixel);
     }
-    div.appendChild(row);
+    table.appendChild(row);
   }
 }
 
-const addMouseEvents = (el) => {
+const addPixelMouseEvents = (el) => {
   el.onmousedown = () => {
     isDrawing = true;
-    el.style.backgroundColor = color;
-    prevColor = color;
+    el.style.backgroundColor = brushColor;
+    prevBrushColor = brushColor;
   }
   el.onmouseup = () => {
     isDrawing = false;
   }
   el.onmouseenter = () => {
     if (isDrawing) {
-      el.style.backgroundColor = color;
-      prevColor = color;
+      el.style.backgroundColor = brushColor;
+      prevBrushColor = brushColor;
     } else {
-      prevColor = el.style.backgroundColor;
+      prevBrushColor = el.style.backgroundColor;
       el.style.backgroundColor = 'lightgray';
     }
   }
   el.onmouseleave = () => {
-    if (!isDrawing) el.style.backgroundColor = prevColor;
+    if (!isDrawing) el.style.backgroundColor = prevBrushColor;
   }
+}
+
+const buildPalette = (table, brushColors) => {
+  const paintRow = document.createElement('tr');
+  for (let color of brushColors) {
+    const paint = document.createElement('td');
+    paint.style.backgroundColor = color;
+    paint.onclick = () => {
+      brushColor = color;
+    }
+    paintRow.appendChild(paint);
+  }
+  table.appendChild(paintRow);
 }
